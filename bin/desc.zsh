448361b616fe4164e0b82fde4d0234fe28580591
Micah Elliott <mde@MicahElliott.com>
2012-12-08 13:47:47 -0800

    use heading info to simply describe a script

---
+++ bin/desc.zsh
@@ -0,0 +1,9 @@
+#! /bin/zsh
+
+# desc — use heading info to simply describe a script
+#
+# Useful as input to git message, too.
+
+script=${1?Must provide script to describe}
+
+sed -nr 1,5p $script |grep — |sed 's/.*— //'
