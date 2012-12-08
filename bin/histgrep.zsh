dcedffb700f9572f95bd10d24692b7f6580e22a2
Micah Elliott <mde@MicahElliott.com>
2012-12-08 11:32:52 -0800

    Color, and escaping grep for - patterns.

--- bin/histgrep.zsh
+++ bin/histgrep.zsh
@@ -3,12 +3,13 @@
 # histgrep — grep for pattern in zsh history, and print human-readable datestamp
 
 pat=${1?Must provide pattern}
+autoload colors; colors
 
-IFS=$'\n' items=( $(grep -P $pat ~/.zsh_history) )
+IFS=$'\n' items=( $(grep -P -- $pat ~/.zsh_history |grep -v ':0;#') )
 
 for i in $items; do
   stamp=$(sed -r 's/: ([0-9]+):.*/\1/' <<<$i)
   day=$(date -d @$stamp '+%F %a %T')
   cmd=$(sed -r 's/: ([0-9]+):0;//' <<<$i)
-  print "$day -- $cmd"
+  print "$fg[yellow]$day$reset_color $cmd"
 done
