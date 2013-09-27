86872b15c488217e8ea3d8a4427abbac6b4f139a
Micah Elliott <mde@MicahElliott.com>
2013-09-27 14:39:31 -0700

    dump docx files

--- bin/docxcat.zsh
+++ bin/docxcat.zsh
@@ -12,3 +12,16 @@ for f in $argv; do
   unoconv -f doc $f
   antiword $f:r.doc
 done
+
+# Not tested yet.
+es=$?
+if [[ $? -gt 0 ]]; then
+  print 'Oops, try catdoc'
+  exit $es
+fi
+
+# Could do it in loop to one file, like in xlsxcat.
+#
+# touch all.txt; for r in *.rtf; do catdoc $r >$r:r.txt; print '='$r:r >>all.txt; cat $r:r.txt >>all.txt ;done
+# http://superuser.com/questions/165978/command-line-tool-in-linux-to-extract-text-from-word-excel-powerpoint-or-co
+# Also try: abiword --to=txt myfile.doc
